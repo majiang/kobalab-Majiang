601f25c1a8048088cdff03231f4754f08bf8e2d5
majiang <majiang@users.noreply.github.com>
2022-09-03 22:47:07 +0900

    Update xiangting.js
    add comments

--- src/js/majiang/util/xiangting.js
+++ src/js/majiang/util/xiangting.js
@@ -2,14 +2,25 @@
  *  Majiang.Util.xiangting
  */
 "use strict";
+/*
+    https://blog.kobalab.net/entry/20150329/1427637532
+    https://blog.kobalab.net/entry/20151216/1450191666
+    https://blog.kobalab.net/entry/20151217/1450357254
+*/
 
 function _xiangting(m, d, g, j) {
+    /*
+        m: 面子
+        d: 搭子
+        g: 孤立牌
+        j: 将頭
+    */
 
     let n = j ? 4 : 5;
-    if (m         > 4) { d += m     - 4; m = 4         }
-    if (m + d     > 4) { g += m + d - 4; d = 4 - m     }
-    if (m + d + g > n) {                 g = n - m - d }
-    if (j) d++;
+    if (m         > 4) { d += m     - 4; m = 4         } // 面子が4を超えた分は搭子とする
+    if (m + d     > 4) { g += m + d - 4; d = 4 - m     } // 搭子もあわせて4を超えた分は単張とする
+    if (m + d + g > n) {                 g = n - m - d } // 孤立牌のうち切らなくてよいもの
+    if (j) d++;                                          // 将頭も搭子と対等にカウント
     return 13 - m * 3 - d * 2 - g;
 }
 
@@ -19,8 +30,8 @@ function dazi(bingpai) {
 
     for (let n = 1; n <= 9; n++) {
         n_pai += bingpai[n];
-        if (n <= 7 && bingpai[n+1] == 0 && bingpai[n+2] == 0) {
-            n_dazi += n_pai >> 1;
+        if (n <= 7 && bingpai[n+1] == 0 && bingpai[n+2] == 0) { // 搭子となりえない位置でカット
+            n_dazi += n_pai >> 1;                               // 左から貪欲に搭子を取り、残りは孤立牌
             n_guli += n_pai  % 2;
             n_pai = 0;
         }
@@ -33,21 +44,25 @@ function dazi(bingpai) {
 }
 
 function mianzi(bingpai, n = 1) {
+    /*
+        bingpai: 兵牌
+        n: ランク (1-origin)
+    */
 
-    if (n > 9) return dazi(bingpai);
+    if (n > 9) return dazi(bingpai); // 搭子を取る
 
-    let max = mianzi(bingpai, n+1);
+    let max = mianzi(bingpai, n+1); // nを使わない
 
-    if (n <= 7 && bingpai[n] > 0 && bingpai[n+1] > 0 && bingpai[n+2] > 0) {
+    if (n <= 7 && bingpai[n] > 0 && bingpai[n+1] > 0 && bingpai[n+2] > 0) { // 順子があれば取る
         bingpai[n]--; bingpai[n+1]--; bingpai[n+2]--;
         let r = mianzi(bingpai, n);
         bingpai[n]++; bingpai[n+1]++; bingpai[n+2]++;
         r.a[0]++; r.b[0]++;
-        if (r.a[0]* 2 + r.a[1] > max.a[0]* 2 + max.a[1]) max.a = r.a;
-        if (r.b[0]*10 + r.b[1] > max.b[0]*10 + max.b[1]) max.b = r.b;
+        if (r.a[0]* 2 + r.a[1] > max.a[0]* 2 + max.a[1]) max.a = r.a;       // a: 向聴数計算式ベース
+        if (r.b[0]*10 + r.b[1] > max.b[0]*10 + max.b[1]) max.b = r.b;       // b: 面子優先
     }
 
-    if (bingpai[n] >= 3) {
+    if (bingpai[n] >= 3) {                                                  // 刻子があれば取る
         bingpai[n] -= 3;
         let r = mianzi(bingpai, n);
         bingpai[n] += 3;
@@ -61,12 +76,15 @@ function mianzi(bingpai, n = 1) {
 
 function mianzi_all(shoupai, jiangpai) {
 
+    // 数牌
     let r = {
         m: mianzi(shoupai._bingpai.m),
         p: mianzi(shoupai._bingpai.p),
         s: mianzi(shoupai._bingpai.s),
+        // {a: [面子, 搭子, 孤立牌], b: [面子, 搭子, 孤立牌]}
     };
 
+    // 字牌
     let z = [0, 0, 0];
     for (let n = 1; n <= 7; n++) {
         if      (shoupai._bingpai.z[n] >= 3) z[0]++;
@@ -78,9 +96,11 @@ function mianzi_all(shoupai, jiangpai) {
 
     let min = 13;
 
+    // 総当たり
     for (let m of [r.m.a, r.m.b]) {
         for (let p of [r.p.a, r.p.b]) {
             for (let s of [r.s.a, r.s.b]) {
+                // 簡易計算 (総当たりにより、いずれかは正しい)
                 let x = [n_fulou, 0, 0];
                 for (let i = 0; i < 3; i++) {
                     x[i] += m[i] + p[i] + s[i] + z[i];
@@ -96,12 +116,13 @@ function mianzi_all(shoupai, jiangpai) {
 
 function xiangting_yiban(shoupai) {
 
+    // 将頭なし
     let min = mianzi_all(shoupai);
 
     for (let s of ['m','p','s','z']) {
         let bingpai = shoupai._bingpai[s];
         for (let n = 1; n < bingpai.length; n++) {
-            if (bingpai[n] >= 2) {
+            if (bingpai[n] >= 2) { // 将頭あり
                 bingpai[n] -= 2;
                 let n_xiangting = mianzi_all(shoupai, true);
                 bingpai[n] += 2;
